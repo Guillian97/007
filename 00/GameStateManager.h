46f91b951bd19da741021a128378e299407c6185
Guillian97 <guillian.griveau@live.fr>
2018-06-08 20:40:17 +0200

    base du jeu faite

---
+++ 00/GameStateManager.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <vector>
+#include <SFML/Graphics.hpp>
+#include "MenuState.h"
+#include "Level1State.h"
+#include "GameOverState.h"
+
+
+class GameStateManager {
+public:
+	static const int MENUSTATE = 0;
+	static const int LEVEL1STATE = 1;
+	static const int GAMEOVERSTATE = 2;
+
+	GameStateManager();
+
+	void SetState(int state);
+	void Update(sf::RenderWindow* window, float frametime);
+	void Draw(sf::RenderWindow* window);
+	void Init();
+	void KeyPressed(sf::Keyboard::Key key);
+	void KeyReleased(sf::Keyboard::Key key);
+
+	~GameStateManager();
+private:
+	std::vector<GameState*> States;
+	int currentState;
+};
---
+++ 00/Level1State.h
@@ -0,0 +1,26 @@
+#pragma once
+#include "GameState.h"
+#include "GameStateManager.h"
+#include "TileMap.h"
+#include "Background.h"
+#include "Player.h"
+#include "Slugger.h"
+
+
+class Level1State : public GameState {
+public:
+	class GameStateManager& gsm;
+	Level1State(class GameStateManager& gsm);
+	void Update(sf::RenderWindow* window, float frametime);
+	void Draw(sf::RenderWindow* window);
+	void Init();
+	void KeyPressed(sf::Keyboard::Key key);
+	void KeyReleased(sf::Keyboard::Key key);
+	~Level1State();
+private:
+	TileMap tileMap;
+	Background background;
+	Player *player;
+	std::vector<Enemy*> enemies;
+	Slugger *s;
+};
